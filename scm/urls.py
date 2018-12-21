308cb3f4944b6aad1160aa63849c67a770dcf8c7
SujoyDU <sujoydebnath.92@gmail.com>
2018-12-21 13:21:29 -0500

    updated urls.py with correct path argument as path doest not accept regular expression.

--- scm/urls.py
+++ scm/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
-    path(r'^snippet/', include('snippets.urls')),
+    path(r'admin/', admin.site.urls),
+    path(r'snippet/', include('snippets.urls')),
 ]
--- snippets/urls.py
+++ snippets/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 # Create a router and register our viewsets with it.
@@ -8,6 +9,7 @@ router.register(r'snippets', views.SnippetViewSet)
 
 
 urlpatterns = [
-    path(r'^', include(router.urls)),
+    path(r'', include(router.urls)),
 ]
 
+# urlpatterns = format_suffix_patterns(urlpatterns)
