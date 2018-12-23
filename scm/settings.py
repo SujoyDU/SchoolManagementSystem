9cee9313d5e8310ad10383bab95e873c2fdb23db
SujoyDU <sujoydebnath.92@gmail.com>
2018-12-22 21:06:55 -0500

    full snippet app

--- scm/settings.py
+++ scm/settings.py
@@ -19,13 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'uqwver=@5xrkzfkbt6ej8k$$qa2-nw$t*o&i_yu!^#_2ab9c#9'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
@@ -72,15 +68,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'scm.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
